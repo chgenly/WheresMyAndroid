9a023c1ea6c800ad360e8e3162dee71f2bed3292
Chris Genly <chris@genly.us>
2010-12-03 18:00:14 -0800

    Moved all files up two levels.

--- src/us/genly/wheresmyandroid/SmsReceiver.java
+++ src/us/genly/wheresmyandroid/SmsReceiver.java
@@ -17,6 +17,8 @@
  */
 package us.genly.wheresmyandroid;
 
+import java.util.StringTokenizer;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -25,6 +27,10 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
+import android.media.AudioManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.telephony.SmsMessage;
@@ -58,9 +64,20 @@ public class SmsReceiver extends BroadcastReceiver {
 				if (body.startsWith("("))
 					return;
 				
-				if (body.toLowerCase().contains(keyword)) {
-					getLocationAndSendReply(context, address);
+				int ix = body.toLowerCase().indexOf(keyword);
+				if (ix == -1)
+					return;
+				String rest = body.substring(ix+keyword.length());
+				String cmd = null;
+				StringTokenizer st = new StringTokenizer(rest);
+			
+				if (st.hasMoreTokens()) {
+					cmd = st.nextToken();
 				}
+				if (cmd == null)
+					getLocationAndSendReply(context, address);
+				else if (cmd.equals("ring"))
+					ringCmd(context);
 			}
 		}
 	}
@@ -107,6 +124,32 @@ public class SmsReceiver extends BroadcastReceiver {
 		return null;
 	}
 
+	private void ringCmd(Context context) {
+		ringerOn(context);
+		volumeUp(context);
+		ring(context);
+	}
+	
+	/**
+	 * Make sure we are not in silent or vibrate mode.
+	 * @param context
+	 */
+	private void ringerOn(Context context) {
+		AudioManager audioManager = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+		audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+	}
+	
+	private void volumeUp(Context context) {
+		AudioManager audioManager = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+		int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_RING);
+		audioManager.setStreamVolume(AudioManager.STREAM_RING, maxVolume, 0);
+	}
+	
+	private void ring(Context context) {
+		Uri uri = RingtoneManager.getActualDefaultRingtoneUri(context, RingtoneManager.TYPE_RINGTONE);
+		Ringtone ringtone = RingtoneManager.getRingtone(context, uri);
+		ringtone.play();
+	}
 	private void sendReply(String phoneNumber, Location l) {
 		String s = "("+l.getLatitude()+", "+l.getLongitude()+")\naltitude="+l.getAltitude()+" speed(m/s)="+l.getSpeed()+"\naccuracy: "+l.getAccuracy();
 		sendSMS(phoneNumber, s);
